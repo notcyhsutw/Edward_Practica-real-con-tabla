f4aa18185b6d31ac56a11eaac6e04a711ada9ae7
Cheng-Yu Hsu <m@cyhsu.me>
2016-06-07 19:43:23 +0800

    original project

--- Practica real con tabla/ViewController.swift
+++ Practica real con tabla/ViewController.swift
@@ -8,18 +8,160 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+
+
+    @IBOutlet var textfield: UITextField!
+
+    @IBOutlet var tableView: UITableView!
+
+    @IBOutlet var swiche: UISwitch!
+
+
+    //MutableArray which has the messages
+    var messages:NSMutableArray = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.None
+
     }
+
+
+
+    @IBAction func swicheaction(sender: AnyObject)
+    {
+
+    }
+
+
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+
+        var ballonview = UIImageView()
+        var label = UILabel()
+
+        let cell:UITableViewCell? = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+
+        cell!.selectionStyle = UITableViewCellSelectionStyle.None
+
+        if (cell == nil) {
+
+
+            ballonview.frame = CGRectZero
+            ballonview.tag = 1
+
+            label.frame = CGRectZero
+            label.backgroundColor = UIColor.clearColor()
+            label.lineBreakMode = NSLineBreakMode.ByWordWrapping
+            label.textAlignment = NSTextAlignment.Left
+            label.numberOfLines = 0
+            label.tag = 2
+            label.sizeToFit()
+            label.font = label.font.fontWithSize(14.0)
+
+            let message = UIView()
+            message.tag = 0
+            message.frame = CGRectMake(0.0, 0.0, cell!.frame.size.width, cell!.frame.size.height)
+            message.addSubview(ballonview)
+            message.addSubview(label)
+
+
+
+            cell!.contentView.addSubview(message)
+        }
+
+        else {
+            //Here i get the error
+            ballonview = (cell?.contentView.viewWithTag(0)?.viewWithTag(1) as! UIImageView)
+            label = (cell?.contentView.viewWithTag(0)?.viewWithTag(2) as! UILabel)
+
+        }
+
+
+        let textt: String = messages.objectAtIndex(indexPath.row) as! String
+
+
+        let size:CGSize = textt.boundingRectWithSize(CGSizeMake(240.0, 480.0), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: [NSFontAttributeName: UIFont.systemFontOfSize(14.0)], context: nil).size
+
+
+
+
+        var ballon:UIImage = UIImage()
+
+
+        if (swiche.on == true ) {
+
+
+            ballonview.frame = CGRectMake(320.0 - (size.width + 28.0), 2.0, size.width + 28.0, CGFloat(ceil(Float(size.height))) + 15.0)
+            ballon = UIImage(named: "green")!.stretchableImageWithLeftCapWidth(24, topCapHeight: 15)
+            label.frame = CGRectMake(307.0 - (size.width + 5.0), 8.0, size.width + 5.0, CGFloat(ceil(Float(size.height))))
+
+
+        } else {
+
+
+            ballonview.frame = CGRectMake(0.0, 2.0, size.width + 28, size.height + 15)
+            ballon = UIImage(named: "grey")!.stretchableImageWithLeftCapWidth(24, topCapHeight: 15)
+            label.frame = CGRectMake(16, 8, size.width + 5, size.height)
+        }
+
+
+        ballonview.image = ballon
+        label.text = textt as String
+
+
+
+        return cell!
+    }
+
+
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+
+        let body:String = messages.objectAtIndex(indexPath.row) as! String
+        let bodysize:CGSize = body.boundingRectWithSize(CGSizeMake(240.0, 480.0), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: [NSFontAttributeName: UIFont.systemFontOfSize(14.0)], context: nil).size
+        return bodysize.height + 15
+
+    }
+
+
+    @IBAction func Okqlq(sender: AnyObject)
+    {
+        if textfield.text != "" {
+            messages.addObject(textfield.text!)
+            tableView.reloadData()
+            textfield.text = ""
+            
+        }
+        
+    }
+    
+    
+    
+    
 }
 
